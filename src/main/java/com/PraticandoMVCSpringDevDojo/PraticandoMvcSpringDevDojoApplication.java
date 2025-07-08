f2c4826410c1ce56dac7048af59b0564d6f9c2d0
Leyva Bezerra <47372809+LeyvinoBezerra@users.noreply.github.com>
2025-07-08 09:44:50 -0300

    recicilize

---
+++ src/main/java/com/PraticandoMVCSpringDevDojo/PraticandoMvcSpringDevDojoApplication.java
@@ -0,0 +1,13 @@
+package com.PraticandoMVCSpringDevDojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticandoMvcSpringDevDojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticandoMvcSpringDevDojoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PraticandoMVCSpringDevDojo/PraticandoMvcSpringDevDojoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PraticandoMVCSpringDevDojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticandoMvcSpringDevDojoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
